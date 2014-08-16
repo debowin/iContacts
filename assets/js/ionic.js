c5be85c50be18d4663252e3d20efb88f11f779e2
debowin <debowin7@gmail.com>
2014-08-16 16:29:36 +0530

    Restangular API calls added

--- assets/js/ionic.js
+++ assets/js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.10
+ * Ionic, v1.0.0-beta.9
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -19,7 +19,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.10'
+  version: '1.0.0-beta.9'
 };
 
 (function(ionic) {
@@ -1983,10 +1983,6 @@ window.ionic = {
    */
   ionic.Platform = {
 
-    // Put navigator on platform so it can be mocked and set
-    // the browser does not allow window.navigator to be set
-    navigator: window.navigator,
-
     /**
      * @ngdoc property
      * @name ionic.Platform#isReady
@@ -2133,7 +2129,7 @@ window.ionic = {
      * @returns {boolean} Whether we are running on iPad.
      */
     isIPad: function() {
-      if( /iPad/i.test(ionic.Platform.navigator.platform) ) {
+      if( /iPad/i.test(window.navigator.platform) ) {
         return true;
       }
       return /iPad/i.test(this.ua);
@@ -2187,7 +2183,7 @@ window.ionic = {
       } else if(this.ua.indexOf('Windows Phone') > -1) {
         platformName = WINDOWS_PHONE;
       } else {
-        platformName = ionic.Platform.navigator.platform && navigator.platform.toLowerCase().split(' ')[0] || '';
+        platformName = window.navigator.platform && navigator.platform.toLowerCase().split(' ')[0] || '';
       }
     },
 
@@ -2226,7 +2222,7 @@ window.ionic = {
       };
       if(versionMatch[pName]) {
         v = this.ua.match( versionMatch[pName] );
-        if(v &&  v.length > 2) {
+        if(v.length > 2) {
           platformVersion = parseFloat( v[1] + '.' + v[2] );
         }
       }
@@ -2626,9 +2622,7 @@ ionic.tap = {
           clonedInput.placeholder = focusInput.placeholder;
           clonedInput.type = focusInput.type;
           clonedInput.value = focusInput.value;
-          clonedInput.style = focusInput.style;
-          clonedInput.className = focusInput.className;
-          clonedInput.classList.add('cloned-text-input');
+          clonedInput.className = 'cloned-text-input';
           clonedInput.readOnly = true;
           focusInput.parentElement.insertBefore(clonedInput, focusInput);
           focusInput.style.top = focusInput.offsetTop;
@@ -2661,18 +2655,10 @@ ionic.tap = {
   },
 
   requiresNativeClick: function(ele) {
-    if(!ele || ele.disabled || (/^(file|range)$/i).test(ele.type) || (/^(object|video)$/i).test(ele.tagName) || ionic.tap.isLabelContainingFileInput(ele) ) {
+    if(!ele || ele.disabled || (/^(file|range)$/i).test(ele.type) || (/^(object|video)$/i).test(ele.tagName) ) {
       return true;
     }
     return ionic.tap.isElementTapDisabled(ele);
-  },
-
-  isLabelContainingFileInput: function(ele) {
-    var lbl = tapContainingElement(ele);
-    if(lbl.tagName !== 'LABEL') return false;
-    var fileInput = lbl.querySelector('input[type=file]');
-    if(fileInput && fileInput.disabled === false) return true;
-    return false;
   },
 
   isElementTapDisabled: function(ele) {
@@ -2963,10 +2949,7 @@ function tapHasPointerMoved(endEvent) {
   }
   var endCoordinates = getPointerCoordinates(endEvent);
 
-  var hasClassList = !!(endEvent.target.classList && endEvent.target.classList.contains);
-  var releaseTolerance = hasClassList & endEvent.target.classList.contains('button') ?
-    TAP_RELEASE_BUTTON_TOLERANCE :
-    TAP_RELEASE_TOLERANCE;
+  var releaseTolerance = (endEvent.target.classList.contains('button') ? TAP_RELEASE_BUTTON_TOLERANCE : TAP_RELEASE_TOLERANCE);
 
   return Math.abs(tapPointerStart.x - endCoordinates.x) > releaseTolerance ||
          Math.abs(tapPointerStart.y - endCoordinates.y) > releaseTolerance;
@@ -5891,16 +5874,6 @@ ionic.views.Scroll = ionic.views.View.inherit({
         Math.abs(self.__decelerationVelocityY) >= self.__minVelocityToKeepDecelerating;
       if (!shouldContinue) {
         self.__didDecelerationComplete = true;
-
-        //Make sure the scroll values are within the boundaries after a bounce,
-        //not below 0 or above maximum
-        if (self.options.bouncing) {
-          self.scrollTo(
-            Math.min( Math.max(self.__scrollLeft, 0), self.__maxScrollLeft ),
-            Math.min( Math.max(self.__scrollTop, 0), self.__maxScrollTop ),
-            false
-          );
-        }
       }
       return shouldContinue;
     };
@@ -6276,9 +6249,9 @@ ionic.scroll = {
 
     if(!lastDrag) return;
 
-    lastDrag.content.style[ionic.CSS.TRANSITION] = '';
-    lastDrag.content.style[ionic.CSS.TRANSFORM] = '';
     ionic.requestAnimationFrame(function() {
+      lastDrag.content.style[ionic.CSS.TRANSITION] = '';
+      lastDrag.content.style[ionic.CSS.TRANSFORM] = '';
       setTimeout(function() {
         lastDrag.buttons && lastDrag.buttons.classList.add('invisible');
       }, 250);
@@ -6442,17 +6415,17 @@ ionic.scroll = {
       if (e.gesture.deltaY < 0 && pixelsPastTop > 0 && scrollY > 0) {
         this.scrollView.scrollBy(null, -pixelsPastTop);
         //Trigger another drag so the scrolling keeps going
-        ionic.requestAnimationFrame(function() {
+        setTimeout(function() {
           self.drag(e);
-        });
+        }.bind(this));
       }
       if (e.gesture.deltaY > 0 && pixelsPastBottom > 0) {
         if (scrollY < this.scrollView.getScrollMax().top) {
           this.scrollView.scrollBy(null, pixelsPastBottom);
           //Trigger another drag so the scrolling keeps going
-          ionic.requestAnimationFrame(function() {
+          setTimeout(function() {
             self.drag(e);
-          });
+          }.bind(this));
         }
       }
     }
@@ -6468,37 +6441,32 @@ ionic.scroll = {
 
       this._currentDrag.currentY = scrollY + pageY - offset;
 
-      // this._reorderItems();
+      this._reorderItems();
     }
   });
 
   // When an item is dragged, we need to reorder any items for sorting purposes
-  ReorderDrag.prototype._getReorderIndex = function() {
+  ReorderDrag.prototype._reorderItems = function() {
     var self = this;
     var placeholder = this._currentDrag.placeholder;
     var siblings = Array.prototype.slice.call(this._currentDrag.placeholder.parentNode.children)
       .filter(function(el) {
-        return el.nodeName === self.el.nodeName && el !== self.el;
+        return el !== self.el;
       });
 
-    var dragOffsetTop = this._currentDrag.currentY;
-    var el;
-    for (var i = 0, len = siblings.length; i < len; i++) {
-      el = siblings[i];
-      if (i === len - 1) {
-        if (dragOffsetTop > el.offsetTop) {
-          return i;
-        }
-      } else if (i === 0) {
-        if (dragOffsetTop < el.offsetTop + el.offsetHeight) {
-          return i;
-        }
-      } else if (dragOffsetTop > el.offsetTop - el.offsetHeight / 2 &&
-                 dragOffsetTop < el.offsetTop + el.offsetHeight * 1.5) {
-        return i;
-      }
+    var index = siblings.indexOf(this._currentDrag.placeholder);
+    var topSibling = siblings[Math.max(0, index - 1)];
+    var bottomSibling = siblings[Math.min(siblings.length, index+1)];
+    var thisOffsetTop = this._currentDrag.currentY;// + this._currentDrag.startOffsetTop;
+
+   if(topSibling && (thisOffsetTop < topSibling.offsetTop + topSibling.offsetHeight)) {
+      ionic.DomUtil.swapNodes(this._currentDrag.placeholder, topSibling);
+      return index - 1;
+    } else if(bottomSibling && thisOffsetTop > (bottomSibling.offsetTop)) {
+      ionic.DomUtil.swapNodes(bottomSibling, this._currentDrag.placeholder);
+      return index + 1;
     }
-    return this._currentDrag.startIndex;
+  
   };
 
   ReorderDrag.prototype.end = function(e, doneCallback) {
@@ -6508,7 +6476,7 @@ ionic.scroll = {
     }
 
     var placeholder = this._currentDrag.placeholder;
-    var finalIndex = this._getReorderIndex();
+    var finalPosition = ionic.DomUtil.getChildIndex(placeholder, placeholder.nodeName.toLowerCase());
 
     // Reposition the element
     this.el.classList.remove(ITEM_REORDERING_CLASS);
@@ -6517,7 +6485,7 @@ ionic.scroll = {
     placeholder.parentNode.insertBefore(this.el, placeholder);
     placeholder.parentNode.removeChild(placeholder);
 
-    this.onReorder && this.onReorder(this.el, this._currentDrag.startIndex, finalIndex);
+    this.onReorder && this.onReorder(this.el, this._currentDrag.startIndex, finalPosition);
 
     this._currentDrag = null;
     doneCallback && doneCallback();
@@ -6933,7 +6901,7 @@ ionic.views.Slider = ionic.views.View.inherit({
       slidePos = new Array(slides.length);
 
       // determine width of each slide
-      width = container.offsetWidth || container.getBoundingClientRect().width;
+      width = container.offsetWidth || container.getBoundClientRect().width;
 
       element.style.width = (slides.length * width) + 'px';
 
@@ -7641,7 +7609,7 @@ ionic.views.Slider = ionic.views.View.inherit({
 (function(ionic) {
 'use strict';
 
-/**
+  /**
    * The SideMenuController is a controller with a left and/or right menu that
    * can be slid out and toggled. Seen on many an app.
    *
@@ -7922,7 +7890,6 @@ ionic.views.Slider = ionic.views.View.inherit({
 
     // Handle a drag event
     _handleDrag: function(e) {
-
       // If we don't have start coords, grab and store them
       if(!this._startX) {
         this._startX = e.gesture.touches[0].pageX;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,119 +1,93 @@
 /**
  * Created by debojeet.chatterjee on 17/7/14.
  */
-var app = angular.module("ionicApp",['ionic','ngAnimate']);
+var app = angular.module("ionicApp",['ionic','ngAnimate','restangular']);
+            //{
+            // name:'Debojeet',
+            // designation: 'CEO',
+            // sex: 'ion-man',
+            // email: 'a@b.com',
+            // address: 'abc',
+            // contact:123
+//         },
+//         {
+//             name:'Akhilesh',
+//             designation: 'CEO',
+//             sex: 'ion-man',
+//             email: 'a@b.com',
+//             address: 'abc',
+//             contact:456
+//         },
+//         {
+//             name:'Priya',
+//             designation: 'CEO',
+//             sex: 'ion-woman',
+//             email: 'a@b.com',
+//             address: 'abc',
+//             contact:789
+//         },
+//         {
+//             name:'Sachin',
+//             designation: 'CEO',
+//             sex: 'ion-man',
+//             email: 'a@b.com',
+//             address: 'abc',
+//             contact:987
+//         },
+//         {
+//             name:'Swapnil',
+//             designation: 'CEO',
+//             sex: 'ion-man',
+//             email: 'a@b.com',
+//             address: 'abc',
+//             contact:342
+//         },
+//         {
+//             name:'Dipesh',
+//             designation: 'CEO',
+//             sex: 'ion-man',
+//             email: 'a@b.com',
+//             address: 'abc',
+//             contact:654
+//         },
+//         {
+//             name:'Pranav',
+//             designation: 'CEO',
+//             sex: 'ion-man',
+//             email: 'a@b.com',
+//             address: 'abc',
+//             contact:879
+//         },
+//         {
+//             name:'Sameer',
+//             designation: 'CEO',
+//             sex: 'ion-man',
+//             email: 'a@b.com',
+//             address: 'abc',
+//             contact:125
+//         }
 
-app.factory("ionFactory",function(){
-    var factory = {};
-    factory.employees = [{
-            name:'Debojeet',
-            designation: 'CEO',
-            sex: 'ion-man',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:123
-        },
-        {
-            name:'Akhilesh',
-            designation: 'CEO',
-            sex: 'ion-man',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:456
-        },
-        {
-            name:'Priya',
-            designation: 'CEO',
-            sex: 'ion-woman',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:789
-        },
-        {
-            name:'Sachin',
-            designation: 'CEO',
-            sex: 'ion-man',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:987
-        },
-        {
-            name:'Swapnil',
-            designation: 'CEO',
-            sex: 'ion-man',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:342
-        },
-        {
-            name:'Dipesh',
-            designation: 'CEO',
-            sex: 'ion-man',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:654
-        },
-        {
-            name:'Pranav',
-            designation: 'CEO',
-            sex: 'ion-man',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:879
-        },
-        {
-            name:'Sameer',
-            designation: 'CEO',
-            sex: 'ion-man',
-            email: 'a@b.com',
-            address: 'abc',
-            contact:125
-        }];
-    factory.add = function(newName,newDesignation,newSex,newEmail,newAddress,newContact){
-        factory.employees.push({
-            name:newName,
-            designation: newDesignation,
-            sex: newSex,
-            email: newEmail,
-            address: newAddress,
-            contact: newContact
-            })
-    };
-    factory.del = function(emp){
-        factory.employees.splice(factory.employees.indexOf(emp), 1);
-    };
-    factory.edit = function(index,empData){
-        factory.employees[index] = empData;
-        //console.log(factory.employees)
-    };
-    factory.reOrder = function(emp,start,end){
-        factory.employees.splice(start, 1);
-        factory.employees.splice(end, 0, emp);
-    };
-    return factory
-});
-
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider,RestangularProvider){
     $stateProvider
         .state('main',{
             url:'/main',
             abstract: true,
-            templateUrl: 'main.html'
+            templateUrl: '/assets/partials/main.html'
         })
         .state('main.home',{
             url: '/home',
             views:{
                 'home-tab': {
-                    templateUrl: 'home.html',
+                    templateUrl: '/assets/partials/home.html',
                     controller: 'homeCtrl'
                 }
             }
-         })
+        })
         .state('main.add',{
             url: '/add',
             views:{
                 'add-tab': {
-                    templateUrl: 'add.html',
+                    templateUrl: '/assets/partials/add.html',
                     controller: 'addCtrl'
                 }
             }
@@ -122,7 +96,7 @@ app.config(function($stateProvider, $urlRouterProvider){
             url: '/edit/:id',
             views:{
                 'home-tab': {
-                    templateUrl: 'edit.html',
+                    templateUrl: '/assets/partials/edit.html',
                     controller: 'editCtrl'
                 }
             }
@@ -131,15 +105,17 @@ app.config(function($stateProvider, $urlRouterProvider){
             url: '/show/:id',
             views:{
                 'home-tab': {
-                    templateUrl: 'show.html',
+                    templateUrl: '/assets/partials/show.html',
                     controller: 'showCtrl'
                 }
             }
         });
         $urlRouterProvider.otherwise("/main/home");
+        RestangularProvider.setBaseUrl("/api");
+        RestangularProvider.setDefaultHeaders({'Content-Type': 'application/json'});
 });
 
-app.controller("addCtrl",function($scope,ionFactory,$state){
+app.controller("addCtrl",function($scope,$state,Restangular,$ionicPopup){
     $scope.data = {
         name: '',
         designation: '',
@@ -155,53 +131,90 @@ app.controller("addCtrl",function($scope,ionFactory,$state){
         return $scope.data.sex=="ion-woman"
     };
     $scope.addPeople = function(){
-        ionFactory.add($scope.data.name,$scope.data.designation,$scope.data.sex,$scope.data.email,$scope.data.address,$scope.data.contact);
+        //ionFactory.add($scope.data.name,$scope.data.designation,$scope.data.sex,$scope.data.email,$scope.data.address,$scope.data.contact);
+        var newPerson = {
+          name: $scope.data.name,
+          designation: $scope.data.designation,
+          contact: parseInt($scope.data.contact),
+          sex: $scope.data.sex,
+          email: $scope.data.email,
+          address: $scope.data.address
+        }
+        Restangular.all("crudlist").post(newPerson)
+        //Display Success Popup on delete.
+        var alertPopup = $ionicPopup.alert({
+         title: 'iContacts',
+         template: 'Add Successful'
+        })
+        alertPopup.then(function(res) {
+         console.log('Add Popup')
+        })
         $state.go('main.home')
     }
 });
 
-app.controller("showCtrl",function($scope,ionFactory,$stateParams,$state){
+app.controller("showCtrl",function($scope,$stateParams,$state,Restangular){
+    $scope.data ={}
     var index = $stateParams.id;
-    $scope.data = {
-        name: ionFactory.employees[index].name,
-        designation: ionFactory.employees[index].designation,
-        sex: ionFactory.employees[index].sex,
-        email: ionFactory.employees[index].email,
-        address: ionFactory.employees[index].address,
-        contact: ionFactory.employees[index].contact
-    };
+    Restangular.one('crudone',index).get().then(function(stuff){
+      $scope.data = stuff
+    })
     $scope.goBack = function(){
         $state.go('main.home')
     }
 });
 
-app.controller("editCtrl",function($scope,ionFactory,$stateParams,$state){
+app.controller("editCtrl",function($scope,$stateParams,$state,Restangular,$ionicPopup){
+    $scope.data ={}
     var index = $stateParams.id;
-    $scope.data = {
-        name: ionFactory.employees[index].name,
-        designation: ionFactory.employees[index].designation,
-        sex: ionFactory.employees[index].sex,
-        email: ionFactory.employees[index].email,
-        address: ionFactory.employees[index].address,
-        contact: ionFactory.employees[index].contact
-    };
+    Restangular.one('crudone',index).get().then(function(stuff){
+      $scope.data = stuff
+    })
     $scope.isMan = function(){
         return $scope.data.sex=="ion-man"
     };
     $scope.isWoman = function(){
         return $scope.data.sex=="ion-woman"
     };
+    $scope.confirmChanges = function(){
+        var confirmPopup = $ionicPopup.confirm({
+             title: 'iContacts',
+             template: 'Are you sure?'
+           });
+           confirmPopup.then(function(res) {
+             if(res) {
+               console.log('Cool');
+               $scope.changePeople();
+             } else {
+               console.log('Whatevs');
+             }
+           });
+    }
     $scope.changePeople = function(){
-        ionFactory.edit(index,$scope.data);
+        //ionFactory.edit(index,$scope.data);     
+        $scope.data.contact = parseInt($scope.data.contact)
+        $scope.data.put()
+        //Display Success Popup on delete.
+        var alertPopup = $ionicPopup.alert({
+            title: 'iContacts',
+            template: 'Edit Successful'
+        })
+        alertPopup.then(function(res) {
+         console.log('Edit Popup');
+        })
         $state.go('main.home')
-    };
-    //console.log($scope.data.sex)
+    }
 });
 
-app.controller("homeCtrl",function($scope,ionFactory){
+app.controller("homeCtrl",function($scope,Restangular,$state,$ionicPopup,$http){
     $scope.data = {showReorder:false,
                     showDelete:false};
-    $scope.employees = ionFactory.employees;
+    $scope.employees = {}
+    setTimeout(function(){
+      Restangular.all('crudlist').getList().then(function(stuff){
+          $scope.employees = stuff
+      })
+    },500)
     $scope.toggleDelete = function(){
         $scope.data.showReorder = false;
         $scope.data.showDelete = !$scope.data.showDelete
@@ -211,12 +224,63 @@ app.controller("homeCtrl",function($scope,ionFactory){
         $scope.data.showReorder = !$scope.data.showReorder
     };
     $scope.moveItem = function(emp, start, end) {
-        ionFactory.reOrder(emp,start,end)
-    };
+      //Awesome Idea: Move one object's details into another and vice versa, like swapping a variable :D
+        if(start==0)
+            start = 1
+        if(end==0)
+            end = 1
+        console.log(start,end)
+      // setTimeout(function(){
+      //   Restangular.all('crudlist').getList().then(function(stuff){
+      //       $scope.employees = stuff
+      //   })
+      // },250)
+};
+    $scope.confirmDelete = function(emp){
+        var confirmPopup = $ionicPopup.confirm({
+             title: 'iContacts',
+             template: 'Are you sure?'
+           });
+           confirmPopup.then(function(res) {
+             if(res) {
+               console.log('Cool');
+               $scope.onItemDelete(emp);
+             } else {
+               console.log('Whatevs');
+             }
+           });
+    }
     $scope.onItemDelete = function(emp) {
-        ionFactory.del(emp)
-    };
+        //$scope.employees.splice(factory.employees.indexOf(emp), 1);
+        //Restangular delete doesn't work as payload is sent.
+        $http({method: 'DELETE', url: '/api/crudone/'+emp.id}).then(function(){
+          //$scope.data.showDelete = false
+          setTimeout(function(){
+            Restangular.all('crudlist').getList().then(function(stuff){
+                $scope.employees = stuff
+            })
+          },500)
+        })
+        //Display Success Popup on delete.
+       var alertPopup = $ionicPopup.alert({
+         title: 'iContacts',
+         template: 'Delete Successful'
+       })
+       alertPopup.then(function(res) {
+         console.log('Delete Popup')
+       })
+   }
     $scope.goEdit = function(index){
         window.location = '#/main/edit/'+index
     }
+    $scope.doRefresh = function(){
+    Restangular.all('crudlist').getList().then(function(stuff) {
+       $scope.employees = stuff;
+     })
+     .finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     })
+    }
+    // setInterval(refresh,1000)
 });
